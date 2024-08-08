1e048cc5e193fca17f05a14185591c7d846c125e
GioSche <134497861+GioCauzzi89@users.noreply.github.com>
2024-08-08 12:20:07 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { useEffect } from 'react';
+import { ConnectButton, useAccount } from '@rainbow-me/rainbowkit';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const { isConnected } = useAccount();
+
+  useEffect(() => {
+    // Se non connesso, apri la finestra di collegamento
+    if (!isConnected) {
+      const button = document.querySelector('button[data-rainbowkit-button]');
+      if (button) button.click(); // Simula il clic sul pulsante Connect Wallet
+    }
+  }, [isConnected]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,9 +26,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title} style={{ marginBottom: "4rem" }}>
-          Welcome to this demo of{" "}
-          <a href="https://www.rainbowkit.com/">RainbowKit</a>
+        <h1 className={styles.title} style={{ marginBottom: '4rem' }}>
+          Welcome to this demo of <a href="https://www.rainbowkit.com/">RainbowKit</a>
         </h1>
 
         <ConnectButton />
