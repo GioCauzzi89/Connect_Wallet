c358bee74ff20542a2e06cb825dabff08e9aacfd
GioSche <134497861+GioCauzzi89@users.noreply.github.com>
2024-08-08 12:32:30 +0200

    b

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,8 @@
-import { useEffect } from 'react';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { useAccount, useConnect, useDisconnect } from 'wagmi';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const { isConnected } = useAccount();
-  const { connect, connectors } = useConnect();
-  const { disconnect } = useDisconnect();
-
-  useEffect(() => {
-    if (!isConnected) {
-      // Se non è connesso, tentare di connettere il primo wallet disponibile
-      if (connectors.length > 0) {
-        connect(connectors[0]); // Connetti al primo wallet disponibile
-      }
-    }
-  }, [isConnected, connectors, connect]);
-
   return (
     <div className={styles.container}>
       <Head>
@@ -30,11 +15,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title} style={{ marginBottom: '4rem' }}>
-          Welcome to this demo of <a href="https://www.rainbowkit.com/">RainbowKit</a>
+        <h1 className={styles.title} style={{ marginBottom: "4rem" }}>
+          Welcome to this demo of{" "}
+          <a href="https://www.rainbowkit.com/">RainbowKit</a>
         </h1>
 
-        {/* Puoi mantenere il pulsante se desideri permettere la connessione/disconnessione manuale */}
         <ConnectButton />
       </main>
     </div>
